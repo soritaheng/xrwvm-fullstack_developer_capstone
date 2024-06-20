cb31a8111cd50de2468b993852917b2e93bac44d
soritaheng <soritahg@gmail.com>
2024-06-20 01:58:34 -0400

    add rest apis and sentiment analyzer

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
+from .restapis import get_request, analyze_review_sentiments, post_review
 
 app_name = 'djangoapp'
 urlpatterns = [
@@ -11,12 +12,23 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
 
-    #path for logout
+    # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
 
+    # path for dealers
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+
+    # path for dealers by state
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'), 
+
+    # path for dealer details
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+
     # path for dealer reviews view
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
 
     # path for add a review view
+    path(route='add_review', view=views.add_review, name='add_review'),
 
     # path for cars view
     path(route='get_cars', view=views.get_cars, name ='getcars'),
